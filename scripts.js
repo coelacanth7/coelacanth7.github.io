83f7b06c09a7644e9709c9040e5dce346d7fe983
Coelacanth7 <galford151@gmail.com>
2017-09-02 11:49:57 -0700

    finished

--- scripts.js
+++ scripts.js
@@ -1,8 +1,28 @@
+
+
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function typeWrite(span){
+
+  var textSpan = $('#' + span).text();
+  var randInt = 0
+
+  for (var i = 0; i < textSpan.length; i++) {
+    randInt += parseInt(randomIntFromInterval(40, 300));
+    var typing = setTimeout(function(y){
+      $('#' + span).append(textSpan.charAt(y));
+    }, randInt, i);
+  }
+}
+
+function wait(span) {
+  setTimeout(function(){
+    typeWrite(span);
+  }, 2000)
+}
+
 $(document).ready(function(){
-
-//https://stackoverflow.com/questions/16344354/how-to-make-blinking-flashing-text-with-css-3
-  (function blink() {
-    $('#animate').fadeOut(500).fadeIn(500, blink);
-  })();
-
+  wait('text')
 })
